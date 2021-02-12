e09fd9936356837207a1fd14f06536fc2968542e
e.bogdanov <e.bogdanov@krtech.ru>
2021-02-12 16:48:45 +0300

    new

--- Liang_20_Lists.java
+++ Liang_20_Lists.java
@@ -3,6 +3,8 @@ collection.add("New York");
 collection.add("Atlanta");
 collection.add("Dallas");
 collection.add("Madison");
+
+/******************** forEach (20.3) ******************/
 collection.forEach(e −> System.out.print(e.toUpperCase() + " "));
 /*
 forEach(
@@ -17,3 +19,11 @@ forEach(
 for (String element: collection)
     System.out.print(element.toUpperCase() + " "); 
 */
+
+/* *********** ITERATOR (20.2) ******************** */
+Iterator<String> iterator = collection.iterator();
+while(iterator.hasNext()) {
+    System.out.print(iterator.next().toUpperCase() + " ");
+}
+
+/********************20.6 The Comparator Interface*************** */
