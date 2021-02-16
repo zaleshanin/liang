389c11adacc99794fc2a7976497469be79375a42
e.bogdanov <e.bogdanov@krtech.ru>
2021-02-16 18:10:40 +0300

    Java EE, glassfish

---
+++ Heffelfinger.java
@@ -0,0 +1,11 @@
+/*
+-----Java EE -------
+- Сервелеты (Servlets)
+- Серверныее страницы Java (Java Server Pages(JSP))
+- Каркас стандартных компонентов Java Server Faces (JSF)
+- Компоненты корпоративных приложений (Enterprise Java Beans (EJB))
+- Службу обмена сообщениями Java (Java Messaging Service (JMS))
+- API Персистентности Java (Java Persistence API (JPA)) - для сохранения сущностей в базе
+- API Java для веб-сервисов XML (Java API for XML Web Sevices (JAX-WS))
+- API Java для веб-сервисов RESTful (Java API for RESTful Web Services (JAX-RS))
+*/
