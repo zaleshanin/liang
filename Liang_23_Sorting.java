69a117911914494686198dfd16875a94aae8d463
zaleshanin <jbo@ua.fm>
2021-04-13 22:08:39 +0300

    sorting

--- Liang_23_Sorting.java
+++ Liang_23_Sorting.java
@@ -40,11 +40,73 @@ public static void insertionSort(int[] list) {
 
 
 // 23.3 Bubble Sort
+/** Bubble sort method */
+public static void bubbleSort(int[] list) {
+    boolean needNextPass = true;
+    for (int k = 1; k < list.length && needNextPass; k++) {
+        // Array may be sorted and next pass not needed
+        needNextPass = false ;
+        for (int i = 0; i < list.length − k; i++) {
+            if (list[i] > list[i + 1]) {
+                // Swap list[i] with list[i + 1]
+                int temp = list[i];
+                list[i] = list[i + 1];
+                list[i + 1] = temp;
+                needNextPass = true; // Next pass still needed
+            }
+        }
+    }
+}
 // 23.4 Merge Sort
+// Merge sort the first half
+int[] firstHalf = new int[list.length / 2];
+System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+mergeSort(firstHalf);
+// Merge sort the second half
+int secondHalfLength = list.length − list.length / 2;
+int[] secondHalf = new int[secondHalfLength];
+System.arraycopy(list, list.length / 2,
+secondHalf, 0, secondHalfLength);
+mergeSort(secondHalf);
+// Merge firstHalf with secondHalf into list
+merge(firstHalf, secondHalf, list);
+}
+}
+/** Merge two sorted lists */
+/** The method for sorting the numbers */
+public static void mergeSort(int[] list) {
+    if (list.length > 1) {// Merge sort the first half
+        int[] firstHalf = new int[list.length / 2];
+        System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+        mergeSort(firstHalf);
+        // Merge sort the second half
+        int secondHalfLength = list.length − list.length / 2;
+        int[] secondHalf = new int[secondHalfLength];
+        System.arraycopy(list, list.length / 2,
+        secondHalf, 0, secondHalfLength);
+        mergeSort(secondHalf);
+        // Merge firstHalf with secondHalf into list
+        merge(firstHalf, secondHalf, list);
+    }
+}
+        
+public static void merge(int[] list1, int[] list2, int[] temp) {
+    int current1 = 0; // Current
+    int current2 = 0; // Current
+    int current3 = 0; // Current
+    while (current1 < list1.length && current2 < list2.length) {
+        if (list1[current1] < list2[current2])
+            temp[current3++] = list1[current1++];
+        else
+            temp[current3++] = list2[current2++];
+    }
+    while (current1 < list1.length)
+        temp[current3++] = list1[current1++];
+    while (current2 < list2.length)
+        temp[current3++] = list2[current2++];
+}
 // 23.5 Quick Sort
 // 23.6 Heap Sort
 // 23.7 Bucket and Radix Sort
 // 23.8 External Sort
-
-
 }
