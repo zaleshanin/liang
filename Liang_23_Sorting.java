cc4ac38cb00497b1ac13cf068448b5bd1e710c09
e.bogdanov <e.bogdanov@krtech.ru>
2021-04-14 17:57:36 +0300

    sotring:merge

--- Liang_23_Sorting.java
+++ Liang_23_Sorting.java
@@ -58,21 +58,6 @@ public static void bubbleSort(int[] list) {
     }
 }
 // 23.4 Merge Sort
-// Merge sort the first half
-int[] firstHalf = new int[list.length / 2];
-System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
-mergeSort(firstHalf);
-// Merge sort the second half
-int secondHalfLength = list.length − list.length / 2;
-int[] secondHalf = new int[secondHalfLength];
-System.arraycopy(list, list.length / 2,
-secondHalf, 0, secondHalfLength);
-mergeSort(secondHalf);
-// Merge firstHalf with secondHalf into list
-merge(firstHalf, secondHalf, list);
-}
-}
-/** Merge two sorted lists */
 /** The method for sorting the numbers */
 public static void mergeSort(int[] list) {
     if (list.length > 1) {// Merge sort the first half
@@ -89,7 +74,8 @@ public static void mergeSort(int[] list) {
         merge(firstHalf, secondHalf, list);
     }
 }
-        
+
+/** Merge two sorted lists */
 public static void merge(int[] list1, int[] list2, int[] temp) {
     int current1 = 0; // Current
     int current2 = 0; // Current
